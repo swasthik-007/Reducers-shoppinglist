67968ba73b4f42558c8f434c1e2d41d648789697
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-20 00:12:39 +0530

    added basic input tag and a button

---
+++ src/components/InputItem/InputItem.jsx
@@ -0,0 +1,19 @@
+import './InputItem.css'
+
+
+function InputItem(){
+    return(
+        <div className="item-input-wrapper">
+            <input
+            className='item-input'
+            type="text"
+            placeholder="Add an Item...."
+            />
+            <button className='add-item-button'>
+                Add
+            </button>
+        </div>
+    )
+}
+
+export default InputItem;
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,9 +1,14 @@
 import Header from "../Header/Header";
+import InputItem from "../InputItem/InputItem";
+import './ShoppingList.css'
 
 function ShoppingList(){
 return(
     <>
      <Header/>
+     <div className="current-shopping-list">
+        <InputItem/>
+     </div>
     </>
 )
 }
