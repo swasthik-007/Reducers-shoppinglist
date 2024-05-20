4f8c7179346457551d3a409cc3ee8c045c0c1c68
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-20 17:15:55 +0530

    Added handlers for adding and removing items

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,19 +1,25 @@
 // CSS imports
 import './ItemList.css';
-
 // Components imports
 import Item from '../Item/Item';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
-function ItemList({ shoppingItems }) {
+// utils
+import { showError } from '../../utils/showToasts';
+
+function ItemList({ shoppingItems, addQuantity, decQuantity }) {
+
     return (
         <div className="shopping-items-wrapper">
             {
                 shoppingItems && shoppingItems.map(item => {
                     return (
                         <div key={item.id} className='items-list'>
-                            <div className='change-quanity add-item'>
+                            <div className='change-quanity add-item'
+                                onClick={() => addQuantity(item.id)}
+                            >
                                 <FontAwesomeIcon icon={faPlus} />
                             </div>
                             <Item 
@@ -21,7 +27,13 @@ function ItemList({ shoppingItems }) {
                                 quantity={item.quantity}
 
                             />
-                            <div className='change-quanity remove-item'>
+                            <div className='change-quanity remove-item'
+                                onClick={() => {
+                                    if(item.quantity == 1) showError(`${item.name} removed from the list`)
+                                    decQuantity(item.id)
+
+                                }}
+                            >
                                 <FontAwesomeIcon icon={faMinus} />
                             </div>
                         </div>
@@ -31,4 +43,5 @@ function ItemList({ shoppingItems }) {
         </div>
     )
 }
-export default ItemList;
+
+export default React.memo(ItemList);
--- src/utils/showToasts.js
+++ src/utils/showToasts.js
@@ -1,14 +1,12 @@
 import { toast } from 'react-toastify';
-
-
 export function showSuccess(message) {
     toast.success(message, {
         position: toast.POSITION.TOP_RIGHT
-    })
+    });
 }
 
 export function showError(message) {
     toast.error(message, {
         position: toast.POSITION.TOP_RIGHT
-    })
+    });
 }
